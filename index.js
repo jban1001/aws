3037d3d56059fe70f1b92a515c67866e9ae0a3cc
jban1001 <115474995+jban1001@users.noreply.github.com>
2023-07-11 02:07:22 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector('.navtooglebutton')
-const menu=document.querySelector('.navmenu');
-const icons=document.querySelector('.menuicons')
-
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-  });
